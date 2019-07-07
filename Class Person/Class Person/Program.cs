dbe439e96970421039a69938280af44121c46859
animkr95 <animkr95@gmail.com>
2019-07-08 01:48:39 +0400

    Person

--- Class Person/Class Person/Program.cs
+++ Class Person/Class Person/Program.cs
@@ -11,107 +11,89 @@ namespace Class_Person
         private string name;
         private string username;
         private int age;
-        private int weight;
+        private float weight;
         private string gender;
 
-        //public Person(string name)
-        //{
-        //    this.name = name;
-        //    Console.Write(" " + name + "  ");
-        //}
-        //public Person(string name, string username) : this(name)
-        //{
-        //    this.username = username;
-        //    Console.WriteLine(username);
-        //}
-        //public Person(int age)
-        //{
-        //    this.age = age;
-        //    Console.Write("age-" + age + "  ");
-        //}
-        //public Person(int age, int weight) : this(age)
-        //{
-        //    this.weight = weight;
-        //    Console.WriteLine("weight-" + weight);
-        //}
-        //public Person(int age, int weight, string gender) : this(age: age, weight: weight)
-        //{
-        //    this.gender = gender;
-        //    Console.WriteLine("   " + gender);
-        //}
-        public string Name
+        public Person() { }
+        public Person(string name, string username, int age, float weight, string gender)
         {
-            get
+            this.name = name;
+            this.username = username;
+            this.age = age;
+            this.weight = weight;
+            this.gender = gender;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine($"Name is {name}");
+        }
+        public void PrintUsername()
+        {
+            Console.WriteLine($"Username is {username}");
+        }
+
+        public void PrintAge()
+        {
+            Console.WriteLine($"Age is {age}");
+        }
+
+        public void PrintWeight()
+        {
+            Console.WriteLine($"Weight is {weight}");
+        }
+
+        public void PrintGender()
+        {
+            Console.WriteLine($"Gender is {gender}");
+        }
+
+        
+        public void Person2name (string newname)
+
+        { name = newname; }
+
+        public void Person2username(string newusername)
+
+        { username = newusername; }
+
+        public void Person2age(int newage)
+
+        { age = newage; }
+
+        public void Person2weight(float newweight)
+
+        { weight = newweight; }
+
+        public void Person2gander  (string newgender)
+
+        { gender = newgender;}
+
+        class Program
+        {
+            static void Main(string[] args)
             {
-                return name;
-            }
-            set
-            {
-                name = value;
+                Person person1 = new Person("Monika", "Belucci", 55, 64, "female");
+               
+                person1.PrintName();
+                person1.PrintUsername();
+                person1.PrintAge();
+                person1.PrintWeight();
+                person1.PrintGender();
+
+                // Console.Write(person1.PrintName() "  ");
+                Console.Write("Person2 name is   ");
+                person1.Person2name(Console.ReadLine());
+                Console.Write("Person2 usernamenameis  ");
+                person1.Person2username(Console.ReadLine());
+                Console.Write("Person2 age is  ");
+                person1.Person2age(int.Parse(Console.ReadLine()));
+                Console.Write("Person2 weight is  ");
+                person1.Person2weight(float.Parse(Console.ReadLine()));
+                Console.Write("Person2 genderis  ");
+                person1.Person2gander(Console.ReadLine());
+                Console.ReadKey();
             }
         }
-        public string Username
-        {
-            get
-            {
-                return username;
-            }
-            set
-            {
-                username = value;
-            }
-        }
-        public int Age
-        {
-            get
-            {
-                return age;
-            }
-            set
-            {
-                age = value;
-            }
-        }
-        public int Weight
-        {
-            get
-            {
-                return weight;
-            }
-            set
-            {
-                weight = value;
-            }
-        }
-        public string Gender
-        {
-            get
-            {
-                return gender;
-            }
-            set
-            {
-                gender = value;
-            }
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           // Person Myperson = new Person("Monika", "Belucci");
-           // Person Myperson1 = new Person(55, 64, "female");
-            Person person1 = new Person();
-            person1.Name = "Zinedine";
-            person1.Username = "Zidane";
-            person1.Age = 47;
-            person1.Weight = 80;
-            person1.Gender = "Male";
-            Console.Write(person1.Name + "  ");
-            Console.WriteLine(person1.Username);
-            Console.Write("age-" + person1.Age);
-            Console.WriteLine("  Weight-" + person1.Weight + "kg");
-            Console.WriteLine("  " + person1.Gender);
-        }
+
     }
 }
