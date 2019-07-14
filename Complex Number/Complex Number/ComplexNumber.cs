1129551cb3a641c8e06020d80b8d4b23954af71b
animkr95 <animkr95@gmail.com>
2019-07-14 23:58:34 +0400

    ComplexNumber

---
+++ Complex Number/Complex Number/ComplexNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Number
+{
+    public struct Complex
+    {
+        public int real;
+        public int imaginary;
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public static Complex operator +(Complex one, Complex two)
+        {
+            return new Complex(one.real + two.real, one.imaginary + two.imaginary);
+        }
+        public override string ToString()
+        {
+            return (String.Format("{0} + {1}i", real, imaginary));
+        }
+        public static Complex operator -(Complex one, Complex two)
+        {
+            return new Complex(one.real - two.real, one.imaginary - two.imaginary);
+        }
+        public static Complex operator *(Complex one, Complex two)
+        {
+            return (new Complex(one.real * two.real - one.imaginary * two.imaginary,
+            one.real * two.imaginary + two.real * one.imaginary));
+        }
+        public static Complex operator/(Complex one, Complex two)
+        {
+            if ((one.real == 0) &&
+            (two.imaginary == 0))
+                throw new DivideByZeroException("Can't divide by zero Complex number");
+
+            int newReal =
+            (one.real * two.real + one.imaginary * two.imaginary) /
+            (two.real * two.real + two.imaginary * two.imaginary);
+            int newImaginary =
+            (one.real * one.imaginary - one.real * two.imaginary) /
+            (two.real * two.real + two.imaginary * two.imaginary);
+
+            return (new Complex(newReal, newImaginary));
+        }
+        public static Complex Add(Complex one, Complex two)
+        {
+            return (one + two);
+        }
+
+        public static Complex Subtract(Complex one, Complex two)
+        {
+            return (one - two);
+        }
+
+        public static Complex Multiply(Complex one, Complex two)
+        {
+            return (one * two);
+        }
+
+        public static Complex Divide(Complex one, Complex two)
+        {
+            return (one / two);
+        }
+    }
+}
---
+++ Complex Number/Complex Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex val1 = new Complex(7, 1);
+            Complex val2 = new Complex(2, 6);
+            // Add both of them
+            Complex res = val1 + val2;
+            Console.WriteLine("First:  {0}", val1);
+            Console.WriteLine("Second: {0}", val2);
+            // display the result
+            Console.WriteLine("Result (Sum): {0}", res);
+
+            Complex Sub = val1 - val2;
+            Console.WriteLine("Result (Sub): {0}", Sub);
+            Complex Multy = val1 * val2;
+            Console.WriteLine("Result (Multy): {0}", Multy);
+            Complex Devide = val1 / val2;
+            Console.WriteLine("Result (Devide): {0}", Devide);
+            Console.ReadLine();
+        }
+    }
+    }
+
