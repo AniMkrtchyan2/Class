c4461893c591f7c0dadee0f5d667bd74aad441d0
animkr95 <animkr95@gmail.com>
2019-07-21 19:55:50 +0400

    Interface Music

---
+++ Interface/Interface/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Interface
+{
+    class Player : IPlaylabel, IRecordable
+    {
+        public void Play()
+        {
+            Console.WriteLine("1 2 3 Start");
+        }
+        public void Pause() {
+            Console.WriteLine("Pause");
+        }
+        public void Stop() {
+            Console.WriteLine("Stop");
+        }
+        public void Record() {
+            Console.WriteLine("Record is start");
+        }
+        void IRecordable.Pause() {
+            Console.WriteLine("Record is Pause");
+        }
+        void IRecordable.Stop() {
+            Console.WriteLine("Record is Stop");
+        }
+
+    }
+}
+
