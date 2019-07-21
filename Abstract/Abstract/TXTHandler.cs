bbe1dfcb381b24122d6e047b0e0a3d9b88c66f14
animkr95 <animkr95@gmail.com>
2019-07-22 03:15:03 +0400

    FileC#

---
+++ Abstract/Abstract/TXTHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class TXTHandler :AbstractHandler
+    {
+        public override void Open()
+        { }
+        public override void Create()
+        { }
+        public override void Chenge()
+        { }
+        public override void Save()
+        { }
+    }
+}
