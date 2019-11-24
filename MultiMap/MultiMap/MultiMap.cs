266ab6eeb2013fdc25462e07fbc83a23cb170097
animkr95 <animkr95@gmail.com>
2019-11-25 01:40:35 +0400

    MultiMap

---
+++ MultiMap/MultiMap/MultiMap.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiMap
+{
+    /// <summary>
+    /// A MultiMap generic collection class wich can store more than one value for a Key
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class MultiMap<TKey, TValue> : IDictionary<TKey, List<TValue>>
+    {
+        public Dictionary<TKey, List<TValue>> MultiMapTenor { get; set; }
+
+        public List<TValue> this[TKey key] { get => this.MultiMapTenor[key]; set => this.MultiMapTenor[key] = value; }
+
+        public ICollection<TKey> Keys { get; set; }
+
+        public ICollection<List<TValue>> Values { get; set; }
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly { get; set; }
+
+        /// <summary>
+        /// MultiMap adds an element
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+
+        public void Add(TKey key, List<TValue> value)
+        {
+            this.MultiMapTenor.Add(key, value);
+        }
+
+        /// <summary>
+        /// Multiap adds an key value
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(KeyValuePair<TKey, List<TValue>> item)
+        {
+            this.MultiMapTenor.Add(item.Key,item.Value);
+        }
+
+        /// <summary>
+        /// Clearing a MutiMap
+        /// </summary>
+
+        public void Clear()
+        {
+            this.MultiMapTenor.Clear();
+        }
+
+        /// <summary>
+        /// Verifies that MultiMap contains the item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+
+        public bool Contains(KeyValuePair<TKey, List<TValue>> item)
+        {
+            return this.MultiMapTenor.Contains(item);
+        }
+
+        /// <summary>
+        /// Verifies that MultiMap contains the key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey(TKey key)
+        {
+            return this.MultiMapTenor.Keys.Contains(key);
+        }
+
+        /// <summary>
+        /// Copies from the specified array index
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(KeyValuePair<TKey, List<TValue>>[] array, int arrayIndex)
+        {
+            if (arrayIndex >= 0 && this.MultiMapTenor != null)
+            {
+                array.Take(arrayIndex).Concat(this.MultiMapTenor);
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, List<TValue>>> GetEnumerator()
+        {
+            return this.MultiMapTenor.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Remove Key in MultiMap
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Remove(TKey key)
+        {
+           return this.MultiMapTenor.Remove(key);
+        }
+
+        /// <summary>
+        /// Remove member in MultiMap
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(KeyValuePair<TKey, List<TValue>> item)
+        {
+            return this.Remove(item);
+        }
+        /// <summary>
+        /// gets the value of MultiMap
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(TKey key, out List<TValue> value)
+        {
+            return this.MultiMapTenor.TryGetValue(key, out value );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        
+    }
+}
---
+++ MultiMap/MultiMap/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiMap
+{
+    class Program
+    {
+        static void Main()
+        {
+            var mm =new Dictionary<int, string>();
+
+            mm.Add(1, "America");
+            mm.Add(2, "Europa");
+            mm.Add(3, "Asia");
+            mm.Add(4, "Africa");
+            Console.WriteLine();
+            foreach (KeyValuePair<int, string> kvp in mm)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+
+            // To get the values alone, use the Values property.
+            ICollection<string> icoll = mm.Values;
+
+            foreach (string s in icoll)
+            {
+                Console.WriteLine("Value = {0}", s);
+            }
+
+            icoll =mm.Values;
+            Console.WriteLine();
+            foreach (string s in icoll)
+            {
+                Console.WriteLine("Key = {0}", s);
+            }
+
+            // Use the Remove method to remove a key/value pair.
+            Console.WriteLine("\nRemove(\"1\")");
+            mm.Remove(1);
+
+            if (!mm.ContainsKey(1))
+            {
+                Console.WriteLine("Key \"1\" is not found.");
+            }
+        }
+    }
+}
