d9a377cec0d50629db43bbac96081f1005edf09c
animkr95 <animkr95@gmail.com>
2019-11-25 15:49:31 +0400

    Airport

---
+++ Airpot/Airpot/Airpot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airpot
+{
+    class Airport
+    {
+        public Airport(string name, string countryCode, Size sizeOfTheAirport)
+        {
+            Name = name;
+            CountryCode = countryCode;
+            SizeOfTheAirport = sizeOfTheAirport;
+        }
+        public Airport(Size sizeOfTheAirport)
+        {
+            SizeOfTheAirport = sizeOfTheAirport;
+        }
+
+        public Airport()
+        {
+        }
+
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public Size SizeOfTheAirport { get; set; }
+        public override string ToString()
+        {
+            return String.Format(" {0}\t---  {1}\t", this.Name, this.CountryCode);
+        }
+    }
+   internal class AirportComparer<T> : IComparer<T>
+        where T:Airport
+    {
+        public int Compare(T x, T y) 
+        {
+            if (x.SizeOfTheAirport > y.SizeOfTheAirport)
+            {
+                return -1;
+            }
+            if (x.SizeOfTheAirport < y.SizeOfTheAirport)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
---
+++ Airpot/Airpot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Airpot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AirportComparer<Airport> cp = new AirportComparer<Airport>();
+            List<Airport> airports = new List<Airport>();
+            airports.Add(new Airport("Singapore Changi", "Singapore", Size.SuperMega));
+            airports.Add(new Airport("Zvartnots", "Armenia", Size.Medium));
+            airports.Add(new Airport("Tokyo Haneda", "Japan", Size.Mega));
+            airports.Add(new Airport("Erebuni", "Armenia", Size.Small));
+            airports.Add(new Airport("Zurich", "Switzerland", Size.Large));
+            
+            
+
+            Console.WriteLine("The World’s Airports \n");
+            Console.WindowWidth = 100;
+            foreach (Airport a in airports)
+                Console.WriteLine(a);
+
+            Console.WriteLine("\n The sort Airport\n");
+            airports.Sort(cp);
+            foreach (Airport a in airports)
+                Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+
+}
