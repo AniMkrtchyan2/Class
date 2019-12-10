e9999a57e03c7ec6d20da3c266796d298a1c4989
animkr95 <animkr95@gmail.com>
2019-12-11 00:13:48 +0400

    OnlineAttendance

---
+++ OnlineAttendance/OnlineAttendance/Online Attendance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAttendance
+{
+    public class Online_Attendance
+    {
+        public event Action<string> BannedName;
+        public List<string> UserName = new List<string>();
+
+        public void Attendance()
+        {
+            var random = new Random();
+            for (int i=0;i< random.Next(0,15);++i)
+            {
+                //Users input their name and it check if names are banned for the organization
+                UserName.Add(Console.ReadLine());
+
+                if ((UserName[i] == "Jack" || UserName[i] == "Steven" || UserName[i] == "Mathew") && BannedName!=null)
+                {
+                    this.BannedName(UserName[i]);
+                   
+
+                }
+                else Console.WriteLine("Welcome to {0}", UserName[i]);
+            }
+        }
+
+
+    }
+}
---
+++ OnlineAttendance/OnlineAttendance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your name ");
+            var attendance = new Online_Attendance();
+            attendance.BannedName += OnBannedName;
+            attendance.Attendance();
+        }
+
+        private static void OnBannedName(string obj)
+        {
+            Console.WriteLine("try again! this UserName is banned");
+        }
+    }
+}
